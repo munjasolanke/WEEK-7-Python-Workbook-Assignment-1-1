59e74a7b7fe5b461ea9b86d8034b6a115b8549b4
Munja Dnyanoba Solanke <54603212+munjasolanke@users.noreply.github.com>
2020-04-02 12:58:10 -0400

    WEEK 7-Python Workbook Assignment-1-1

---
+++ Assignment1_1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Week-7-Homework
+#Assignment-1-1
+
+import pandas as pd
+Location = "C:/Users/munja/Downloads/all_040_in_01.P1.csv"
+df = pd.read_csv(Location)
+
+
+# In[2]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
